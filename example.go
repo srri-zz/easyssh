ac43fde5694a34e168cab8edcc1744962b08ab40
Isagani Mendoza <gani.mendoza@gmail.com>
2015-10-01 09:10:24 +0800

    Update example.go

--- example.go
+++ example.go
@@ -6,11 +6,15 @@ import (
 	"github.com/stevenbrichards/easyssh"
 )
 
+//Usage: either pass pswd or keypath to id_rsa
+//Example: easyssh -user root -pswd yourPswd -host 192.168.56.101 -cmd uptime
+//Example: easyssh -user root -keypath id_rsa -host 192.168.56.101 -cmd uptime
+
 var user = flag.String("user", "", "username")
 var pswd = flag.String("pswd", "", "password")
 var host = flag.String("host", "", "host server or IP address")
 var cmd = flag.String("cmd", "", "command to run")
-var keypath = flag.String("keypath", "", "keypath")
+var keypath = flag.String("keypath", "", "keypath to id_rsa")
 
 func main() {
 	flag.Parse()
@@ -31,6 +35,4 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(result)
-
-	fmt.Scanln()
 }
